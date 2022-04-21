2281ea9a66d7512342bde27e791e56b352475913
solhee-hwang <solhee0912@kookmin.ac.kr>
2022-04-21 23:37:55 +0900

    코드 정렬

--- src/Component/Modal.jsx
+++ src/Component/Modal.jsx
@@ -1,24 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom';
-import classes from './Modal.module.css'
-const Background = props => {
-    return <div className={classes.background} onClick={props.onClose}/>
-}
-const ModalWindow = props => {
-    return (
-        <div className={classes.modal}>
-            <div>{props.children}</div>
-        </div>
-    )
-}
-const portalElement = document.getElementById('modal-window');
+import React from "react";
+import ReactDOM from "react-dom";
+import classes from "./Modal.module.css";
+const Background = (props) => {
+  return <div className={classes.background} onClick={props.onClose} />;
+};
+const ModalWindow = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div>{props.children}</div>
+    </div>
+  );
+};
+const portalElement = document.getElementById("modal-window");
 const Modalnew = (props) => {
-    return (
-        <>
-            {ReactDOM.createPortal(<ModalWindow>{props.children}</ModalWindow>, portalElement)}
-            {ReactDOM.createPortal(<Background onClose={props.onClose}/>, portalElement)}
-        </>
-    )
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <ModalWindow>{props.children}</ModalWindow>,
+        portalElement
+      )}
+      {ReactDOM.createPortal(
+        <Background onClose={props.onClose} />,
+        portalElement
+      )}
+    </>
+  );
 };
 
-export default Modalnew
+export default Modalnew;
